4348ce8b8618b5aa63e60fda1b5e2cebce44a21c
ian_the_dev <marketing@apcreativecorp.com>
2026-05-21 16:35:55 +0800

    just checking the username

--- components/employee/approvals/approval-report-details-summary.tsx
+++ components/employee/approvals/approval-report-details-summary.tsx
@@ -16,7 +16,7 @@ const dateFormatter = new Intl.DateTimeFormat("en-US", {
   month: "short",
   day: "numeric",
   year: "numeric",
-})
+}) 
 
 function StatusBadge({ status }: { status: string }) {
   return (
